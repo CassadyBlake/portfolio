0fbc44a8f10eea85cf35a0ec663e5892a37facf4
CassadyBlake <cassady.lillstrom@gmail.com>
2020-02-19 14:12:10 -0800

    setup first code component

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,53 +1,62 @@
 <template>
-  <div class="layout">
-    <div class="btn-box">
-        <div>
-      <!-- <v-btn
-        v-show="showSideButtons"
-        fab
-        class="tall-btn"
-        @mouseover="scroll_left"
-        @mouseout="clearIntervalScroll"
-      > -->
-            <i class="material-icons">keyboard_arrow_left</i>
+<div class="container">
+    <div class="layout info">
+        this is where i explain myself... lorem ipsum quatar lorem ipsum quatar lorem ipsum quatar
+        this is where i explain myself... lorem ipsum quatar lorem ipsum quatar lorem ipsum quatar
+        this is where i explain myself... lorem ipsum quatar lorem ipsum quatar lorem ipsum quatar
+        this is where i explain myself... lorem ipsum quatar lorem ipsum quatar lorem ipsum quatar
+        this is where i explain myself... lorem ipsum quatar lorem ipsum quatar lorem ipsum quatar
+        this is where i explain myself... lorem ipsum quatar lorem ipsum quatar lorem ipsum quatar
+    </div>
+    <div class="layout controlls">
+        <div class="btn-box" @click="addEvent">
+            <i class="material-icons">add_circle</i>
+        </div>
+        <div class="btn-box" @click="removeEvent">
+            <i class="material-icons">remove_circle</i>
         </div>
     </div>
-    <div class="overlay-container">
-      <div class="overlay-left"></div>
-      <div class="overlay-right"></div>
-      <div class="history-box">
-        <ol class="ol-box">
-          <li v-for="(event, index) in currentTimeLineEvents" :key="index">
-            <p>
-              <span class="event-type">{{ event.type }}</span>
-            </p>
-            <span v-if="event.event_completed" class="point-filled"></span>
-            <span v-else class="point-empty"></span>
-          </li>
-          <li></li>
-        </ol>
-      </div>
+    <div class="layout content">
+        <div class="btn-box scroll">
+            <div 
+                v-if="showSideButtons"
+                @mouseover="scrollLeft"
+                @mouseout="clearIntervalScroll"
+            >
+                <i class="material-icons">keyboard_arrow_left</i>
+            </div>
+        </div>
+        <div class="overlay-container">
+            <div class="overlay-left"></div>
+            <div class="overlay-right"></div>
+            <div class="history-box">
+                <div class="line"></div>
+                <ol class="ol-box">
+                <li v-for="(event, index) in currentTimeLineEvents" :key="index">
+                    <p>
+                    <span class="event-type">{{ event.type }}</span>
+                    </p>
+                    <span v-if="event.event_completed" class="point-filled"></span>
+                    <span v-else class="point-empty"></span>
+                </li>
+                <li></li>
+                </ol>
+            </div>
+        </div>
+        <div class="btn-box scroll">
+            <div 
+                v-if="showSideButtons"
+                @mouseover="scrollRight"
+                @mouseout="clearIntervalScroll"
+            >
+                <i class="material-icons">keyboard_arrow_right</i>
+            </div>
+        </div>
     </div>
-    <div class="btn-box">
-      <!-- <v-btn
-        v-show="showSideButtons"
-        fab
-        class="tall-btn"
-        @mouseover="scroll_right"
-        @mouseout="clearIntervalScroll"
-      > -->
-      <div>
-        <i class="material-icons">keyboard_arrow_right</i>
-      </div>
-    </div>
-  </div>
+</div>
 </template>
 
 <script>
-// import GetAllEventsByPhaseId from "@/graphql/GetAllEventsByPhaseId.gql";
-
-// import { mapGetters } from "vuex";
-// import { util } from "@/mixins/util";
 
 export default {
   name: "historical-timeline",
@@ -56,7 +65,14 @@ export default {
       intervalScroll: null,
       intervalValue: 0,
       showSideButtons: false,
-      currentTimeLineEvents: [
+      types: ['field', 'tech', 'drafting'],
+      startingEvents: [
+          { type: 'field', event_completed: true },
+          { type: 'tech', event_completed: true },
+          { type: 'field', event_completed: false },
+          { type: 'drafting', event_completed: true }
+      ],
+      timeLineEvents: [
           { type: 'field', event_completed: true },
           { type: 'tech', event_completed: true },
           { type: 'field', event_completed: false },
@@ -66,42 +82,50 @@ export default {
   },
 //   mixins: [util],
   watch: {
-    currentTimeLineEvents() {
-      const historyBoxWidth = document.querySelector(".history-box")
-        .clientWidth;
+    timeLineEvents() {
+      const historyBoxWidth = document.querySelector(".history-box").clientWidth;
+      console.log("history box width:", historyBoxWidth);
       const olWidth = document.querySelector(".ol-box").scrollWidth;
+      console.log("ol box width:", olWidth);
       this.showSideButtons = historyBoxWidth < olWidth ? true : false;
     }
   },
   computed: {
-    // ...mapGetters("prism", [
-    //   "getActiveUsers"
-    //   // ...
-    // ])
+    currentTimeLineEvents() {
+        let newArray = this.timeLineEvents;
+        return newArray.sort( (a, b) => {
+            // console.log(a, b);
+            return (a.event_completed === b.event_completed) 
+                ? 0 
+                : a.event_completed 
+                    ? -1 
+                    : 1;
+        });
+    }
   },
   mounted() {
     const historyBoxWidth = document.querySelector(".history-box").clientWidth;
+    console.log("history box width:", historyBoxWidth);
     const olWidth = document.querySelector(".ol-box").scrollWidth;
+    console.log("ol box width:", olWidth);
     this.showSideButtons = historyBoxWidth < olWidth ? true : false;
   },
   methods: {
-    userNameFromId(userId) {
-      let userName = "";
-      this.getActiveUsers.forEach(user => {
-        if (user.id === userId) {
-          userName = user.first_name + " " + user.last_name;
-        }
-      });
-      return userName;
+    addEvent() {
+        let newEvent = { type: this.types[Math.floor(Math.random() * this.types.length)], event_completed: [true, false][Math.floor(Math.random() * 2)] };
+        this.timeLineEvents.push(newEvent);
     },
-    scroll_left() {
+    removeEvent() {
+        this.timeLineEvents.pop();
+    },
+    scrollLeft() {
       let content = document.querySelector(".history-box");
       this.intervalScroll = setInterval(function() {
         this.intervalValue = 5;
         content.scrollLeft -= this.intervalValue;
       }, 10);
     },
-    scroll_right() {
+    scrollRight() {
       let content = document.querySelector(".history-box");
       this.intervalScroll = setInterval(function() {
         this.intervalValue = 5;
@@ -117,40 +141,82 @@ export default {
 <style lang="scss" scoped>
 
 
-/* timeline list */
-.layout {
-    align-items: center;
+.container {
+    border: solid 1px rgba(201, 201, 201, 0.50);
+    margin: 0 10rem;
+    min-width: 1200px;
+    display: inline-flex;
     flex-direction: row;
+    flex-wrap: nowrap;
+}
+
+.layout {
+    align-items: flex-end;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    width: 65%;
+
+    &.info {
+        height: 300px;
+        width: 30%;
+        background-color: rgba(201, 201, 201, 0.50);
+        padding: 2rem;
+        overflow-y: scroll;
+    }
+    &.content {
+        padding: 50px 0;
+        min-width: 750px;
+    }
+    &.controlls {
+        display: inline-flex;
+        flex-direction: column;
+        align-items: flex-start;
+        width: 5%;
+        padding: 2rem;
+    }
 }
 
 .btn-box {
+    position: relative;
     display: inline-flex;
-    flex-grow: 1;
-    padding: 0 1rem;   
+    height: 30px;
+    
+    &.scroll {
+        top: 10px;
+        margin: 1rem 1rem;
+    }
 
     div {
-        width: 50px;
-        height: 50px;
+        width: 25px;
+        height: 25px;
         border-radius: 100px;
         box-shadow: 2px 2px 5px rgb(170, 170, 170); 
     }
 }
 
 .history-box {
-    width: 1000px;
+    width: 600px;
     overflow-x: hidden;
+    display: inline-flex;
+
+    .line {
+        position: absolute;
+        background-color: #374f5b71;
+        margin: 100px 0 100px 0;
+        height: 2px;
+        width: 100%;
+    }
 }
 .overlay-container {
   position: relative;
   display: inline-flex;
-  flex-grow: 10;
 }
 .overlay-left,
 .overlay-right {
   position: absolute;
   top: 0;
   bottom: 0;
-  width: 50px;
+  width: 25px;
   height: 100%;
   background-image: linear-gradient(to right, #ffffff, rgba(255, 0, 0, 0));
   z-index: 5;
@@ -162,9 +228,9 @@ export default {
 }
 ol {
   white-space: nowrap;
-  background: #374f5b;
+//   background: #374f5b71;
   margin-right: 100px;
-  margin: 100px 0 50px 0;
+  margin: 100px 0 100px 0;
   height: 2px;
   list-style: none;
   -ms-box-orient: horizontal;
@@ -176,49 +242,53 @@ ol {
   display: flex;
 }
 li {
-  padding-left: 10%;
-  width: 100px;
-  line-height: 100px;
-  min-width: 100px;
+  left: -25px;
+  padding-left: 30px;
+  width: 50px;
+  line-height: 50px;
+  min-width: 50px;
   flex-grow: 1;
-  background: #374f5b;
   position: relative;
-}
-li p {
-  position: relative;
-}
-li p .event-type {
-  font-size: 12px;
-  position: absolute;
-  transform-origin: 0 0;
-  transform: rotate(-45deg);
-  top: -50px;
-  left: -15px;
-}
-li .point-filled,
-li .point-empty {
-  top: -10px;
-  content: "";
-  width: 20px;
-  height: 20px;
-  border: 2px solid #fff;
-  border-radius: 20px;
-  background-color: #60879b;
-  position: absolute;
-  box-shadow: 2px 2px 3px #374f5b;
-  z-index: 1;
-}
-li .point-empty {
-  transform-origin: 10 10;
-  transform: scale(0.75);
-  border: none;
-  background-color: #fff;
+
+  p {
+    position: relative;
+
+    .event-type {
+        font-size: 14px;
+        position: absolute;
+        transform-origin: 0 0;
+        transform: rotate(-45deg);
+        top: -75px;
+        left: 30px;
+    }
+  }  
+  .point-filled,
+  .point-empty {
+    top: -6px;
+    content: "";
+    width: 10px;
+    height: 10px;
+    border: 2px solid #fff;
+    border-radius: 10px;
+    background-color: #60879b;
+    position: absolute;
+    box-shadow: 2px 2px 3px #374f5b;
+    z-index: 1;
+  }
+  .point-empty {
+    top: -4px;
+    transform-origin: 10 10;
+    transform: scale(0.75);
+    border: none;
+    background-color: #fff;
+  }
 }
 
 /* tall button style */
 .tall-btn {
+  display: inline-flex;
   height: 50px;
-  width: 50px;
+  width: 25px;
   border-radius: 25px;
   padding: 0;
   margin: 50px 0 0 0;
@@ -228,6 +298,7 @@ li .point-empty {
 }
 
 .material-icons {
-    font-size: 48px;
+    font-size: 24px;
+    color: #374f5b;
 }
 </style>
